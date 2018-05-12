ae3ceff4fba2729489dc009bdc6c3da57f1f8026
I3ryI3e <jgrunho@gmail.com>
2018-05-12 14:49:31 +0100

    Constants Final

--- Model/Constants.java
+++ Model/Constants.java
@@ -3,15 +3,15 @@ package Model;
 import java.io.Serializable;
 
 public interface Constants extends Serializable{
-    public enum Enemy_Attack{
+    enum Enemy_Attack{
         NONE,LADDER,BATTERING_RAM,SIEGE_TOWER,SWORD
     }
     
-    public int N_ENEMY_SQUARES=3;
-    public int N_ENEMY_CIRCLES=1;
-    public int N_ENEMY_CLOSE_COMBAT=1;
-    public int TAM_TRACKS_ENEMY=N_ENEMY_CIRCLES+N_ENEMY_CLOSE_COMBAT+N_ENEMY_SQUARES;
-    public int N_PLAYER_SQUARES=5;
-    public int N_TUNNEL_SPACES=2;
-    public int CLOSE_COMBAT_POSITION_POWER = 4;
+    final int N_ENEMY_SQUARES=3;
+    final int N_ENEMY_CIRCLES=1;
+    final int N_ENEMY_CLOSE_COMBAT=1;
+    final int TAM_TRACKS_ENEMY=N_ENEMY_CIRCLES+N_ENEMY_CLOSE_COMBAT+N_ENEMY_SQUARES;
+    final int N_PLAYER_SQUARES=5;
+    final int N_TUNNEL_SPACES=2;
+    final int CLOSE_COMBAT_POSITION_POWER = 4;
 }
